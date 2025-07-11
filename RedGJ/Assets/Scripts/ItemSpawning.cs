cd74001d5959698cec7c86c37ebc5283f324194b
Joyce <140150177+joyce-yong@users.noreply.github.com>
2025-07-12 07:03:48 +0800

    .

--- RedGJ/Assets/Scripts/ItemSpawning.cs
+++ RedGJ/Assets/Scripts/ItemSpawning.cs
@@ -14,16 +14,16 @@ public class ItemSpawning : MonoBehaviour
         else
         {
 
-            float pos_x = Random.Range(-3.0f, 3.0f);
+            float pos_x = Random.Range(-2.5f, 2.5f);
             int index = Random.Range(0, gm.Length); 
 
             // Spawn object
-            GameObject spawned = Instantiate(gm[index], new Vector3(pos_x, 6.0f, 0.1f), Quaternion.identity);
+            GameObject spawned = Instantiate(gm[index], new Vector3(pos_x, 3.0f, 0.1f), Quaternion.identity);
 
             Rigidbody2D rb = spawned.GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                rb.gravityScale = 0.01f; 
+                rb.gravityScale = 0.005f; 
             }
 
             timer = 1.0f;
