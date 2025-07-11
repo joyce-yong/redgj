4c802c5f55f91d7aa9f305ca17407250576e6822
tumuj <167149338+tumuj@users.noreply.github.com>
2025-07-12 02:01:24 +0800

    1

---
+++ RedGJ/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            if (touchPos.x < 0)
+            {
+                rb.AddForce(Vector2.left * moveSpeed);
+            }
+            else
+            {
+                rb.AddForce(Vector2.right * moveSpeed);
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+}
+
