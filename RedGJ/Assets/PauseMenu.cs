5477ccf9870cd7b42087b471ef330e24de568a91
tumuj <167149338+tumuj@users.noreply.github.com>
2025-07-12 12:07:52 +0800

    ui

--- RedGJ/Assets/PauseMenu.cs
+++ RedGJ/Assets/PauseMenu.cs
@@ -20,20 +20,24 @@ public class PauseMenu : MonoBehaviour
     public void Pause() 
     {
         pauseMenu.SetActive(true);
+        Time.timeScale = 0;
     }
 
     public void Home()
     {
         SceneManager.LoadScene("Main Menu");
+        Time.timeScale = 1;
     }
 
     public void Resume()
     {
         pauseMenu.SetActive(false);
+        Time.timeScale = 1;
     }
 
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Time.timeScale = 1;
     }
 }
