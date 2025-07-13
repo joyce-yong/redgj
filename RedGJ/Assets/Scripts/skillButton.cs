abd4dec465a0a5c7fe0a488607e2a9c8bfdc184a
Friedrice130 <yappp130@gmail.com>
2025-07-13 08:38:23 +0800

    adjust skill trigger size

--- RedGJ/Assets/Scripts/skillButton.cs
+++ RedGJ/Assets/Scripts/skillButton.cs
@@ -42,7 +42,7 @@ public class skillButton : MonoBehaviour
 	{
 		GameState.IsPausedBySkillTrigger = true;
 
-		yield return new WaitForSeconds(5f);
+		yield return new WaitForSeconds(4f);
 
 		GameState.IsPausedBySkillTrigger = false;
 		
